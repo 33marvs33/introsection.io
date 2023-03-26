3d531d80c0dcfb00222484e5ad5f70256e8c438f
33marvs33 <108669397+33marvs33@users.noreply.github.com>
2023-03-27 00:13:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelectorAll(".toggle");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector('.overlay');
+const toggleDropdown = document.querySelectorAll(".arrow");
 
 navToggle.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -15,19 +16,31 @@ navToggle.forEach((btn) => {
   });
 });
 
-// const dropdown = document.querySelectorAll(".dropdown");
-const toggleDropdown = document.querySelectorAll(".arrow");
-// const dropdownLinks = document.querySelectorAll('.dropdown-links');
+
 
 
 toggleDropdown.forEach(arrow => {
   arrow.addEventListener('click', (e) => {
     const targetDropdown = e.currentTarget.parentElement.parentElement.children[1];
     const dropdownLinks = e.currentTarget.parentElement.parentElement.children[1].children[0];
-    const heights = dropdownLinks.getBoundingClientRect().height
+    const heights = dropdownLinks.getBoundingClientRect().height;
+     const downArrow = e.currentTarget.parentElement.childNodes[3];
+      const upArrow = e.currentTarget.parentElement.childNodes[5];
+    
+//     if else statement//
     targetDropdown.getBoundingClientRect().height <= 0 ? targetDropdown.style.height = heights + 20 + 'px': targetDropdown.style.height = '0px';
+    if(targetDropdown.getBoundingClientRect().height <= 0) {
+      upArrow.classList.remove('hidden');
+      downArrow.classList.add('hidden');
+    }else {
+      upArrow.classList.add('hidden');
+      downArrow.classList.remove('hidden');
+    }
+  });
+    
   })
 })
+
 
 window.addEventListener('resize', () => {
   if(window.innerWidth >= 900) {
